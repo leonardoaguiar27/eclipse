8d33f39d76f0799a124c15cc2a304dba58d50cfe
leonardoaguiar27 <134439632+leonardoaguiar27@users.noreply.github.com>
2024-01-26 10:27:58 -0300

    Adicionei arquivos via Upload

---
+++ HelloGitHub/src/br/com/leonardoaguiar/git/HelloGit.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package br.com.leonardoaguiar.git;
+
+/**
+ * @author Leonardo Aguiar
+ * Exemplo integração do Eclipse com o GitHub
+ */
+public class HelloGit {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+
+	}
+
+}
